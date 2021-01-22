2024b342a2fb02b2a9231bb8ad061c072ed3a77b
Bryan Nelson <bryananelsonjr@gmail.com>
2021-01-22 12:48:10 -0500

    Initial Commit

---
+++ CourseView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CourseView
+//
+//  Created by Bryan Nelson on 1/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
